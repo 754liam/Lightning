e5987a2228390ddcdc4f14cb5efda1ddb087e330
754liam <182143381+754liam@users.noreply.github.com>
2026-05-08 16:00:19 -0400

    Implemented ordertypes.h header for structuring order types.
    Included two classes - MarketOrder and LimitOrder - to classify incoming client requests and modularize them accordingly.

---
+++ engine/ordertypes.h
@@ -0,0 +1,47 @@
+#include <iostream>
+#include <cstdint>
+
+enum class Side{Buy, Sell};
+
+class LimitOrder{
+    public:
+        LimitOrder(uint64_t share_count, Side side, uint64_t price, uint64_t id, uint64_t timestamp){
+            this->share_count = share_count;
+            this->price = price;
+            this->side = side;
+            this->id = id;
+            this->timestamp = timestamp;
+        }
+        ~LimitOrder() = default;
+        uint64_t getId()        const { return id; }
+        Side     getSide()      const { return side; }
+        uint64_t getShareCount()  const { return share_count; }
+        uint64_t getPrice()     const { return price; }
+        uint64_t getTimestamp() const { return timestamp; }
+    private:
+        uint64_t share_count;
+        uint64_t price;
+        uint64_t timestamp;
+        Side side;
+        uint64_t id;
+};
+
+class MarketOrder{
+    public:
+        MarketOrder(uint64_t share_count, Side side, uint64_t id, uint64_t timestamp){
+            this->share_count = share_count;
+            this->side = side;
+            this->id = id;
+            this->timestamp = timestamp;
+        }
+        ~MarketOrder() = default;
+        uint64_t getId()        const { return id; }
+        Side     getSide()      const { return side; }
+        uint64_t getShareCount()  const { return share_count; }
+        uint64_t getTimestamp() const { return timestamp; }
+    private:
+        uint64_t share_count;
+        uint64_t timestamp;
+        Side side;
+        uint64_t id;
+};
