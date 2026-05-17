dfb796254c5f95a20d580600e034bb660f393f74
754liam <182143381+754liam@users.noreply.github.com>
2026-05-17 15:19:58 -0400

    Implemented more matcher logic.

--- engine/matcher.cpp
+++ engine/matcher.cpp
@@ -2,11 +2,11 @@
 
 Matcher::Matcher() {};
 
-void Matcher::matchBidMarket(OrderBook & order_book, LimitOrder & limit_order){
+void Matcher::matchBidMarket(OrderBook & order_book, MarketOrder & market_order){
     if (order_book.asksEmpty()){
         return;
     }
-    uint64_t share_count = limit_order.getShareCount();
+    uint64_t share_count = market_order.getShareCount();
     while (share_count > 0 && !order_book.asksEmpty()){
         LimitOrder & ask = order_book.frontAsk();
         if (ask.getShareCount() > share_count){
@@ -29,5 +29,37 @@ void Matcher::matchBidMarket(OrderBook & order_book, LimitOrder & limit_order){
 
 }
 
+void Matcher::matchAskMarket(OrderBook & order_book, MarketOrder & market_order){
+    if (order_book.bidsEmpty()){
+        return;
+    }
+    uint64_t share_count = market_order.getShareCount();
+    while (share_count > 0 && !order_book.bidsEmpty()){
+        LimitOrder & bid = order_book.frontBid();
+        if (bid.getShareCount() > share_count){
+            bid.lowerShares(share_count);
+            order_book.bidCleanup();
+            break;
+        }
+        else if (bid.getShareCount() == share_count){
+            order_book.popBid();
+            order_book.bidCleanup();
+            break;
+        }
+        else if (bid.getShareCount() < share_count){
+            share_count -= bid.getShareCount();
+            order_book.popBid();
+        }
+    }
+}
+
+void Matcher::matchAskLimit(OrderBook & order_book, LimitOrder & limit_order){
+    // to-do: if ask can not be fulfilled fully, it must wait. thoughts: condition variables + threads?
+
+}
+
+void Matcher::matchBidLimit(OrderBook & order_book, LimitOrder & limit_order){
+    
+}
 
 
